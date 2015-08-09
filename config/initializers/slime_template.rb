0e51a24c1845fc0c5f2b1252c86662aefe3d9e88
NikolaevAV <nuriko@inbox.ru>
2015-08-10 00:47:40 +0300

    fix вывода в angular в файлах slim

---
+++ config/initializers/slime_template.rb
@@ -0,0 +1,6 @@
+Rails.application.assets.register_engine('.slim', Slim::Template)
+Slim::Engine.set_default_options(
+  attr_list_delims: {'(' => ')', '[' => ']'},
+  code_attr_delims: {'(' => ')', '[' => ']'},
+  format: :html
+)
