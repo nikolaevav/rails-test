94ca73e4770916a8c198fc8f9a84a2958412b932
NikolaevAV <nuriko@inbox.ru>
2015-08-09 22:53:39 +0300

    Обновлена и настроена конфигурация приложения для запуска

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require materialize-sprockets
+//= require main
--- config/application.rb
+++ config/application.rb
@@ -4,13 +4,25 @@ require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  Bundler.require(*Rails.groups(assets: %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
 
 module RailsTest
   class Application < Rails::Application
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -51,12 +63,21 @@ module RailsTest
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    # config.active_record.whitelist_attributes = false
 
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    I18n.enforce_available_locales = false
+    config.i18n.default_locale = :ru
+    I18n.locale = :ru
+    I18n.default_locale = :ru
+
+    config.time_zone = 'Moscow'
+    config.active_record.default_timezone = :local
+
+    # Rails 2.3 Ruby 4
+    config.active_record.raise_in_transactional_callbacks = true
+    config.app_generators.scaffold_controller :responders_controller
   end
 end
